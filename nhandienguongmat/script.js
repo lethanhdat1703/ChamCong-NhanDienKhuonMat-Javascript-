ecfef7f89e7c729562dd2909534f22243757b8fb
Le Thanh Dat <lethanhdat1732002@gmail.com>
2024-03-25 17:17:27 +0700

    new commit

--- nhandienguongmat/script.js
+++ nhandienguongmat/script.js
@@ -1,53 +1,83 @@
-const video=document.getElementById('videoElm');
+const video = document.getElementById("video");
+const model = [];
+Promise.all([
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  faceapi.nets.ageGenderNet.loadFromUri("/models"),
+]).then(startVideo);
 
-const loadFaceApi =async()=>{
-    await faceapi.nets.faceLandmark68Net.loadFromUri('./models');
-    await faceapi.nets.faceRecognitionNet.loadFromUri('./models');
-    await faceapi.nets.tinyFaceDetector.loadFromUri('./models');
-    await faceapi.nets.faceExpressionNet.loadFromUri('./models');
-    await faceapi.nets.ageGenderNet.loadFromUri('./models');
-}   
-
-function getCameraStream(){
-    if(navigator.mediaDevices.getUserMedia){
-        navigator.mediaDevices.getUserMedia({video:{}})
-        .then(stream=>{
-            video.srcObject=stream;
-        });
-    }
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
+  );
 }
 
-video.addEventListener('playing',()=>{
-    const canvas =faceapi.createCanvasFromMedia(video);
-    document.body.append(canvas);
-    const displaySize={
-        width: video.videoWidth,
-        height: video.videoHeight,
-    }
-    setInterval(async()=>{
-     const detects =  await faceapi.detectAllFaces(video,new faceapi.TinyFaceDetectorOptions())
-        .withFaceLandmarks()
-        .withFaceExpressions()
-        .withAgeAndGender();
-     const resizeDetects=faceapi.resizeResults(detects,displaySize);
-     canvas.getContext('2d').clearRect(0,0,displaySize.width,displaySize.height);
-     faceapi.draw.drawDetections(canvas,resizeDetects);
-     faceapi.draw.drawFaceLandmarks(canvas,resizeDetects);
-     faceapi.draw.drawFaceExpressions(canvas,resizeDetects);
-     resizeDetects.forEach(face => {
-        const { age, gender, genderProbability } = face;
-        const genderText = `${gender}`;
-        const ageText = `${Math.round(age)} years`;
-        const text = `${genderText}, ${ageText}`;
-        const box = face.detection.box;
-        const drawOptions = {
-            anchor: { x: box.x, y: box.y },
-            fontSize: 20,
-            textColor: 'white'
-        };
-        new faceapi.draw.DrawTextField([text], box, drawOptions).draw(canvas);
-    });
-}, 300);
-});
-loadFaceApi().then(getCameraStream);
+function getLabeledFaceDescriptions() {
+  const labels = ["messi", "ronaldo","j97","Dat","NTN Vlog"];
+  return Promise.all(
+    labels.map(async (label) => {
+      const descriptions = [];
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`./labels/${label}.png`);
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detections.descriptor);
+      }
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+    })
+  );
+}
 
+video.addEventListener("play", async () => {
+  const labeledFaceDescriptors = await getLabeledFaceDescriptions();
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { 
+    width: video.width,
+    height: video.height 
+    };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions()
+      .withAgeAndGender()
+      .withFaceDescriptors();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    resizedDetections.forEach((face) => {
+      const { age, gender } = face;
+      const genderText = `${gender}`;
+      const ageText = `${Math.round(age)} years`;
+      const text = `${genderText}, ${ageText}`;
+      const box = face.detection.box;
+      const drawOptions = {
+        anchor: { x: box.x, y: box.y },
+        fontSize: 20,
+      };
+      new faceapi.draw.DrawTextField([text], box, drawOptions).draw(canvas);
+    });
+    const results = resizedDetections.map((d) => {
+      return faceMatcher.findBestMatch(d.descriptor);
+    });
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: result,
+      });
+      drawBox.draw(canvas);
+    });
+  }, 100);
+});
